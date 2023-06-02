27c9617843219117b9de6856fe4edcf9df392479
Guandaru254 <leroykim82@gmail.com>
2023-06-02 10:39:13 +0300

    Add files via upload

--- designations.js
+++ designations.js
@@ -13,14 +13,9 @@ const dbConfig = {
     database : 'dartgame_db',
 };
 
-/*pool.query ('SELECT * FROM players', (err, result, fields)  => {
-    if (err) {
-        return console.log (err);
-    }
-    return console.log (result);
-})*/
-
 // Create a router instance
+
+app.use(express.json());
 
 const connection = mysql.createConnection(dbConfig);
 
@@ -50,8 +45,23 @@ app.get ('/',  (req, res) => {
 
     
 app.get ('/:id',  (req, res) => {
-    const paymentId = req.params.id;
-    connection.query('SELECT * FROM payments WHERE id = ?',[paymentId],(err,result) => {
+    const Designation_ID = req.params.id;
+    connection.query('SELECT * FROM designations WHERE Designation_ID = ?',[Designation_ID],(err,result) => {
+        if (err) {
+            console.error('Error retrieving designation');
+            return res.status(500).json({error : 'Error retrieving designation'});
+        }
+        if (result.length === 0) {
+            return res.status(404).json({error : 'No designation found'});
+        }
+        
+        res.json(result[0]);
+    });
+});
+
+app.get ('/:id',  (req, res) => {
+    const Payment_ID = req.params.id;
+    connection.query('SELECT * FROM payments WHERE Payment_ID = ?',[Payment_ID],(err,result) => {
         if (err) {
             console.error('Error retrieving payment');
             return res.status(500).json({error : 'Error retrieving payment'});
@@ -66,7 +76,7 @@ app.get ('/:id',  (req, res) => {
 
 app.post ('/', (req,res) => {
     const { Designation_ID , Designation_Role , Hire_Date } = req.body;
-    const query = 'INSERT INTO designations ( Designation_ID , Designation_Role , Hire_Date ) VALUES (?,?,?,?) ';
+    const query = 'INSERT INTO designations ( Designation_ID , Designation_Role , Hire_Date ) VALUES (?,?,?) ';
     connection.query(query, [ Designation_ID , Designation_Role , Hire_Date ], (err,result) => {
         if (err) {
       console.error ('Error inserting designations data into database');
@@ -75,6 +85,40 @@ app.post ('/', (req,res) => {
       res.json({success : true, message : 'Designations data successfully inserted'});
     });
 });
+
+
+app.put('/', (req,res) => {
+    const { Player_ID, Player_Name, Phone_Number, Age } = req.body;
+    const updatedPlayerData = { Player_ID , Player_Name , Phone_Number , Age  };
+    connection.query ('UPDATE players SET ? WHERE Player_ID = ?', [updatedPlayerData, Player_ID], (err,result) => {
+        if (err) {
+            console.error ('Error updating player');
+            return res.status (500).json({error : 'Error updating player'});
+        }
+        if (result.affectedRows === 0) {
+            return res.status (404).json({error : 'Player not found'});
+        }
+        res.json ({message : 'Player updated successfully'});
+    });
+});
+
+
+app.delete ('/:id', (req,res) => {
+    const Player_ID = req.params.id;
+    const query = 'DELETE FROM players WHERE Player_ID = ?';
+    connection.query (query, [Player_ID], (err,result) => {
+        if (err) {
+            console.log ('Error deleting player');
+            return res.status(500).json({error : 'Error deleting player'});
+        }
+        if (result.affectedRows === 0) {
+            return res.status (404).json({error : 'Player not found'});
+        }
+        res.json({message : 'Player deleted successfully'});
+    });
+});
+
+
 process.on('SIGINT', () => {
     connection.end();
     process.exit();
