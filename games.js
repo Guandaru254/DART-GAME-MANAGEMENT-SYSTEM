a67ac7b403fead407bda7301f95d0a5bd6d1fe2d
Guandaru254 <leroykim82@gmail.com>
2023-06-02 10:37:27 +0300

    Add files via upload

--- games.js
+++ games.js
@@ -43,19 +43,65 @@ app.get ('/',  (req, res) => {
         });
     });
 
+    app.get ('/:id',  (req, res) => {
+        const Game_ID = req.params.id;
+        connection.query('SELECT * FROM games WHERE Game_ID = ?',[Game_ID],(err,result) => {
+            if (err) {
+                console.error('Error retrieving game', err);
+                return res.status(500).json({error : 'Error retrieving game'});
+            }
+            if (result.length === 0) {
+                return res.status(404).json({error : 'No game found'});
+            }
+            
+            res.json(result[0]);
+        });
+    });
+
 
 app.post ('/', (req,res) => {
     const { Game_ID , Player_ID , Game_Type , Game_Status } = req.body;
-    const query = 'INSERT INTO games (Game_ID , Player_ID , Game_Type , Game_Status) VALUES (?,?,?,?) ';
-    connection.query(query, [Game_ID , Player_ID , Game_Type , Game_Status], (err,result) => {
-        if (err) {
-      console.error ('Error inserting game data into database');
-      return res.status (500).json({error : 'Error inserting game data into database'});
+    const query = 'INSERT INTO games (Game_ID , Player_ID , Game_Type , Game_Status) VALUES (?,?,?,?)'; 
+    connection.query(query, [ Game_ID , Player_ID , Game_Type , Game_Status ], (err,result) => {
+    if (err) {
+        console.error ('Error inserting payment data into database');
+        return res.status (500).json({error : 'Error inserting payment data into database'});
         }
       res.json({success : true, message : 'Game data successfully inserted'});
     });
 });
 
+
+app.put('/', (req,res) => {
+    const { Player_ID, Player_Name, Phone_Number, Age } = req.body;
+    const updatedPlayerData = { Player_ID , Player_Name , Phone_Number , Age  };
+    connection.query ('UPDATE players SET ? WHERE Player_ID = ?', [updatedPlayerData, Player_ID], (err,result) => {
+        if (err) {
+            console.error ('Error updating player');
+            return res.status (500).json({error : 'Error updating player'});
+        }
+        if (result.affectedRows === 0) {
+            return res.status (404).json({error : 'Player not found'});
+        }
+        res.json ({message : 'Player updated successfully'});
+    });
+});
+
+
+app.delete ('/:id', (req,res) => {
+    const Player_ID = req.params.id;
+    const query = 'DELETE FROM players WHERE Player_ID = ?';
+    connection.query (query, [Player_ID], (err,result) => {
+        if (err) {
+            console.log ('Error deleting player');
+            return res.status(500).json({error : 'Error deleting player'});
+        }
+        if (result.affectedRows === 0) {
+            return res.status (404).json({error : 'Player not found'});
+        }
+        res.json({message : 'Player deleted successfully'});
+    });
+});
 
 process.on('SIGINT', () => {
     connection.end();
