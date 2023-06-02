3f05c5a9ede4f77d48335e855da25db04ec1a531
Guandaru254 <leroykim82@gmail.com>
2023-06-02 10:37:01 +0300

    Add files via upload

--- scores.js
+++ scores.js
@@ -43,20 +43,20 @@ app.get ('/',  (req, res) => {
     });
 
     
-app.get ('/:id',  (req, res) => {
-    const paymentId = req.params.id;
-    connection.query('SELECT * FROM payments WHERE id = ?',[PAYOO4],(err,result) => {
-        if (err) {
-            console.error('Error retrieving payment');
-            return res.status(500).json({error : 'Error retrieving payment'});
-        }
-        if (result.length === 0) {
-            return res.status(404).json({error : 'No payment found'});
-        }
-        
-        res.json(result[0]);
+    app.get ('/:id',  (req, res) => {
+        const Score_ID = req.params.id;
+        connection.query('SELECT * FROM scores WHERE Score_ID = ?',[Score_ID],(err,result) => {
+            if (err) {
+                console.error('Error retrieving score', err);
+                return res.status(500).json({error : 'Error retrieving score'});
+            }
+            if (result.length === 0) {
+                return res.status(404).json({error : 'No score found'});
+            }
+            
+            res.json(result[0]);
+        });
     });
-});
 
 
 app.post ('/', (req,res) => {
@@ -68,6 +68,37 @@ app.post ('/', (req,res) => {
       return res.status (500).json({error : 'Error inserting score data into database'});
         }
       res.json({success : true, message : 'Score data successfully inserted'});
+    });
+});
+
+
+app.put('/', (req,res) => {
+    const { Player_ID, Player_Name, Phone_Number, Age } = req.body;
+    const updatedPlayerData = { Player_ID , Player_Name , Phone_Number , Age  };
+    connection.query ('UPDATE players SET ? WHERE Player_ID = ?', [updatedPlayerData, Player_ID], (err,result) => {
+        if (err) {
+            console.error ('Error updating player');
+            return res.status (500).json({error : 'Error updating player'});
+        }
+        if (result.affectedRows === 0) {
+            return res.status (404).json({error : 'Player not found'});
+        }
+        res.json ({message : 'Player updated successfully'});
+    });
+});
+
+app.delete ('/:id', (req,res) => {
+    const Player_ID = req.params.id;
+    const query = 'DELETE FROM players WHERE Player_ID = ?';
+    connection.query (query, [Player_ID], (err,result) => {
+        if (err) {
+            console.log ('Error deleting player');
+            return res.status(500).json({error : 'Error deleting player'});
+        }
+        if (result.affectedRows === 0) {
+            return res.status (404).json({error : 'Player not found'});
+        }
+        res.json({message : 'Player deleted successfully'});
     });
 });
 
