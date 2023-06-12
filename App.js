c18c1a794aafc89db054bbf8f901c646bb93cb03
Guandaru254 <leroykim82@gmail.com>
2023-06-12 12:13:02 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import SidebarMenu from './sidenav'
+import AppRouter  from './AppRouter';
+
+React.render(
+  <React.StrictMode>
+    <AppRouter/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 function App() {
 
